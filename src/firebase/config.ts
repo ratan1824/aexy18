fc1ac29d2b17761bd044795351b6438936d0cd4a
kollabathula ratan <kollabathularatan@gmail.com>
2025-10-18 18:12:17 +0000

    i changed the api key, thats why my features arent working ig. look at t

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-1733550761-8a423",
   "appId": "1:13577320397:web:a9414f9307bdc9abc9b684",
-  "apiKey": "AIzaSyBKkF1Y4swXyKXLRWBb9Y2NNrQbFzPnA5w",
+  "apiKey": "AIzaSyBGF-czKbYWcM4rZV3LGDJrrzRfOxio7gU",
   "authDomain": "studio-1733550761-8a423.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "13577320397"
